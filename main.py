ba896ccf2cd77b45e03a89869c53d6676b8a8f46
Carlo <carlo@MacBook-Pro-de-Carlo.local>
2024-12-11 23:19:17 -0500

    WIP: starting CRUD

---
+++ main.py
@@ -0,0 +1,17 @@
+from fastapi import FastAPI
+
+app = FastAPI()
+
+
+
+@app.get('/')
+async def root():
+    return {"message": "Hello World"}
+  
+@app.get('/url')
+async def url():
+    return {"url": "https://www.google.com"}
+  
+root()
+
+# fastapi dev main.py   
